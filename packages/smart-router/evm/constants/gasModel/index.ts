292d3630486ca37a2245297840c2e1ad15ae5a36
Mind1995Star <daisukenagata0209@gmail.com>
2023-09-01 15:57:53 +0900

    create and update: pancake-swap commit again

---
+++ packages/smart-router/evm/constants/gasModel/index.ts
@@ -0,0 +1,34 @@
+import { ChainId, Token } from '@pancakeswap/sdk'
+import {
+  ethereumTokens,
+  bscTokens,
+  bscTestnetTokens,
+  goerliTestnetTokens,
+  polygonZkEvmTokens,
+  polygonZkEvmTestnetTokens,
+  zkSyncTestnetTokens,
+  zksyncTokens,
+  lineaTestnetTokens,
+  arbitrumGoerliTokens,
+  arbitrumTokens,
+  baseGoerliTokens,
+} from '@pancakeswap/tokens'
+
+export const usdGasTokensByChain = {
+  [ChainId.ETHEREUM]: [ethereumTokens.usdt],
+  [ChainId.GOERLI]: [goerliTestnetTokens.usdc],
+  [ChainId.BSC]: [bscTokens.busd],
+  [ChainId.BSC_TESTNET]: [bscTestnetTokens.busd],
+  [ChainId.ARBITRUM_ONE]: [arbitrumTokens.usdc],
+  [ChainId.ARBITRUM_GOERLI]: [arbitrumGoerliTokens.usdc],
+  [ChainId.POLYGON_ZKEVM]: [polygonZkEvmTokens.usdt],
+  [ChainId.POLYGON_ZKEVM_TESTNET]: [polygonZkEvmTestnetTokens.usdt],
+  [ChainId.ZKSYNC]: [zksyncTokens.usdc],
+  [ChainId.ZKSYNC_TESTNET]: [zkSyncTestnetTokens.usdc],
+  [ChainId.LINEA_TESTNET]: [lineaTestnetTokens.usdc],
+  [ChainId.BASE_TESTNET]: [baseGoerliTokens.usdc],
+} satisfies Record<ChainId, Token[]>
+
+export * from './v2'
+export * from './v3'
+export * from './stableSwap'
